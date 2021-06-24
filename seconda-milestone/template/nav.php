abcdfb181ae004cbdfcb2b1b83ce6f8271817be6
domingo79 <andria140779@yahoo.it>
2021-06-24 18:24:16 +0200

    aggiunto campo ricerca e bottone

--- seconda-milestone/template/nav.php
+++ seconda-milestone/template/nav.php
@@ -1,4 +1,11 @@
 <body>
     <nav>
-        <h1><?= $title ?></h1>
+        <div class="logo">
+            <h1><?= $title ?></h1>
+        </div>
+        <div class="serch">
+            <input type="text" name="generi" id="generi">
+            <button type="submit">Cerca</button>
+        </div>
+
     </nav>
