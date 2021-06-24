8313663d61b333b4ace97f02525d23c7c3ee20da
domingo79 <andria140779@yahoo.it>
2021-06-24 20:31:46 +0200

    corretto percorso di css prima milestone

--- prima-milestone/index.php
+++ prima-milestone/index.php
@@ -11,7 +11,7 @@ include __DIR__ . '/dataBase/db_dischi.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./asset/css//style.css">
+    <link rel="stylesheet" href="./asset/css/style.css">
     <title><?= $title ?></title>
 </head>
 
